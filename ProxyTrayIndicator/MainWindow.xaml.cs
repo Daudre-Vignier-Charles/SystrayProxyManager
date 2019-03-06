f128dea96437a23622c48cfee038040d75025209
Charles DAUDRE-VIGNIER <charles-cucm_desktop@daudre.vignier.fr>
2019-03-06 13:46:30 +0100

    update

--- ProxyTrayIndicator/MainWindow.xaml.cs
+++ ProxyTrayIndicator/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Net;
+using System.Drawing;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -20,9 +22,75 @@ namespace ProxyTrayIndicator
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private string key = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\";
+        private string name = "ProxyEnable";
+        private bool proxySet = true;
+        System.Windows.Forms.NotifyIcon notifyIcon = new System.Windows.Forms.NotifyIcon()
+        { 
+            Visible = true
+        };
+        System.Windows.Forms.ContextMenu menu = new System.Windows.Forms.ContextMenu();
+        Button button = new Button()
+        {
+            Content = "Exit"
+        };
+        private static System.Timers.Timer timer;
+
+        
+        public   MainWindow()
         {
             InitializeComponent();
+            button.Click += ExitClick;
+            menu.MenuItems.Add("Exit", new EventHandler(ExitClick));
+            menu.MenuItems.Add("Copyright", new EventHandler(ShowC));
+            notifyIcon.ContextMenu = menu;
+            notifyIcon.Click += Click;
+            SetTimer();
+        }
+
+        private void Click(Object sender, EventArgs e)
+        {
+            System.Windows.Forms.MouseEventArgs z = (System.Windows.Forms.MouseEventArgs)e;
+            if (z.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                if (proxySet)
+                    Microsoft.Win32.Registry.SetValue(key, name, 0, Microsoft.Win32.RegistryValueKind.DWord);
+                else
+                    Microsoft.Win32.Registry.SetValue(key, name, 1, Microsoft.Win32.RegistryValueKind.DWord);
+            }
+        }
+
+        private void SetTimer()
+        {
+            timer = new System.Timers.Timer(1000); //Timer goes off every 1 seconds
+            timer.Elapsed += Timer_Elapsed;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            int res = (int)Microsoft.Win32.Registry.GetValue(key, name, 8);
+            if (res == 1)
+            {
+                proxySet = true;
+                notifyIcon.Icon = Resource.on;
+            }
+            else
+            {
+                proxySet = false;
+                notifyIcon.Icon = Resource.off;
+            }
+        }
+
+        private void ExitClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ShowC(object sender, EventArgs e)
+        {
+            MessageBox.Show(Resource.License, "Copyrights and licenses");
         }
     }
 }
