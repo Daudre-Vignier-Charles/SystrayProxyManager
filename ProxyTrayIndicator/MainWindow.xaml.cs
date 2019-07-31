118d78536b2ac9313eea461ba592fd44cb615b4d
Charles Daudré-Vignier <daudre.vignier.charles@yandex.com>
2019-07-31 15:57:50 +0200

    clean code

--- ProxyTrayIndicator/MainWindow.xaml.cs
+++ ProxyTrayIndicator/MainWindow.xaml.cs
@@ -1,19 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Net;
-using System.Drawing;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace ProxyTrayIndicator
 {
@@ -46,7 +33,7 @@ namespace ProxyTrayIndicator
             }
         };
 
-        public   MainWindow()
+        public MainWindow()
         {
             if (!mutex.WaitOne(TimeSpan.Zero, true))
             {
