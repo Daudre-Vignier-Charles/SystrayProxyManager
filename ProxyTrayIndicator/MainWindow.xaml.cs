65c637d3a944d9ebef331b10351b37136c0cf106
Charles Daudré-Vignier <daudre.vignier.charles@yandex.com>
2019-08-01 11:35:23 +0200

    clean code and update access modifier

--- ProxyTrayIndicator/MainWindow.xaml.cs
+++ ProxyTrayIndicator/MainWindow.xaml.cs
@@ -4,28 +4,23 @@ using System.Windows;
 
 namespace ProxyTrayIndicator
 {
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         // Mutex is used to allow only one instance of ProxyTrayIndicator
-        static System.Threading.Mutex mutex = new System.Threading.Mutex(true, "{8F6E0AC4-B9A1-45fd-AC6F-73F04E6BDE8F}");
+        private static System.Threading.Mutex mutex = new System.Threading.Mutex(true, "{8F6E0AC4-B9A1-45fd-AC6F-73F04E6BDE8F}");
         private static string key = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\";
         private static string name = "ProxyEnable";
         private bool proxySet = true;
-        System.Windows.Forms.NotifyIcon notifyIcon = new System.Windows.Forms.NotifyIcon()
+        private System.Windows.Forms.NotifyIcon notifyIcon = new System.Windows.Forms.NotifyIcon()
         {
             Text = (string)Microsoft.Win32.Registry.GetValue(key, "ProxyServer", "Aucune"),
             Visible = true
         };
-        System.Windows.Forms.ContextMenu menu = new System.Windows.Forms.ContextMenu();
+        private System.Windows.Forms.ContextMenu menu = new System.Windows.Forms.ContextMenu();
         private static System.Timers.Timer timer;
-        /// <summary>
-        ///  Microsoft Windows Internet Settings
-        /// </summary>
-        System.Diagnostics.Process proc = new System.Diagnostics.Process()
+        private System.Diagnostics.Process proc = new System.Diagnostics.Process()
         {
+            //  Microsoft Windows Internet Settings
             StartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + "inetcpl.cpl ,4")
             {
                 CreateNoWindow = true,
@@ -54,8 +49,6 @@ namespace ProxyTrayIndicator
         /// <summary>
         /// Switch proxy state on tray icon click
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void Click(Object sender, EventArgs e)
         {
             if (((System.Windows.Forms.MouseEventArgs)e).Button == System.Windows.Forms.MouseButtons.Left)
@@ -72,7 +65,7 @@ namespace ProxyTrayIndicator
         /// </summary>
         private void SetTimer()
         {
-            timer = new System.Timers.Timer(1000); //Timer goes off every 1 seconds
+            timer = new System.Timers.Timer(1000);
             timer.Elapsed += Timer_Elapsed;
             timer.AutoReset = true;
             timer.Enabled = true;
@@ -81,8 +74,6 @@ namespace ProxyTrayIndicator
         /// <summary>
         /// Check proxy state and update tray icon
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
             if ((int)Microsoft.Win32.Registry.GetValue(key, name, 8) == 1)
@@ -98,37 +89,16 @@ namespace ProxyTrayIndicator
             notifyIcon.Text = (string)Microsoft.Win32.Registry.GetValue(key, "ProxyServer", "no proxy server");
         }
 
-        /// <summary>
-        /// Clean and exit app
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ExitClick(object sender, EventArgs e)
-        {
+        private void ExitClick(object sender, EventArgs e) =>
             this.Close();
-        }
 
-        /// <summary>
-        /// Show copyright notice
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ShowC(object sender, EventArgs e)
-        {
+        private void ShowC(object sender, EventArgs e) =>
             MessageBox.Show(Resource.License, "Copyrights and licenses");
-        }
 
-        /// <summary>
-        /// Open Microsoft Windows Internet Settings
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void LaunchIEParamClick(object sender, EventArgs e)
-        {
+        private void LaunchIEParamClick(object sender, EventArgs e) =>
             proc.Start();
-        }
 
-        void OnClose(Object sender, EventArgs e)
+        private void OnClose(Object sender, EventArgs e)
         {
             mutex.ReleaseMutex();
             notifyIcon.Dispose();
