80578c95b4910d409e440a790ea810d9cd4c63a7
Charles DAUDRE-VIGNIER <charles-csdev@daudre.vignier.fr>
2019-12-06 14:05:45 +0100

    added proxy management

--- ProxyTrayIndicator/MainWindow.xaml.cs
+++ ProxyTrayIndicator/MainWindow.xaml.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
-
 
 namespace ProxyTrayIndicator
 {
     public partial class MainWindow : Window
     {
         // Mutex is used to allow only one instance of ProxyTrayIndicator
-        private static System.Threading.Mutex mutex = new System.Threading.Mutex(true, "{8F6E0AC4-B9A1-45fd-AC6F-73F04E6BDE8F}");
+        private bool close = false;
+        private string savePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\proxysSave";
+        private static System.Threading.Mutex mutex = new System.Threading.Mutex(false, "{100008-B9A1-45fd-AC6F-73F04E6BDE8F}");
         private static string key = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\";
-        private static string name = "ProxyEnable";
+        private static string regProxyEnable = "ProxyEnable";
+        private static string regProxyServer = "ProxyServer";
         private bool proxySet = true;
         private System.Windows.Forms.NotifyIcon notifyIcon = new System.Windows.Forms.NotifyIcon()
         {
@@ -17,6 +21,7 @@ namespace ProxyTrayIndicator
             Visible = true
         };
         private System.Windows.Forms.ContextMenu menu = new System.Windows.Forms.ContextMenu();
+        private System.Windows.Forms.MenuItem proxyMenu = new System.Windows.Forms.MenuItem() { Text = "Set proxy" };
         private static System.Timers.Timer timer;
         private System.Diagnostics.Process proc = new System.Diagnostics.Process()
         {
@@ -27,23 +32,25 @@ namespace ProxyTrayIndicator
                 UseShellExecute = false,
             }
         };
+        private List<Proxy> proxys = new List<Proxy>();
 
         public MainWindow()
         {
+            this.Closing += EventClosing;
+            this.Closed += EventClosed;
             if (!mutex.WaitOne(TimeSpan.Zero, true))
             {
-                MessageBox.Show("only one instance at a time");
+                System.Windows.Forms.MessageBox.Show("only one instance at a time");
                 this.Close();
                 return;
             }
-            Closing += OnClose;
             InitializeComponent();
-            menu.MenuItems.Add("Show IE settings", new EventHandler(LaunchIEParamClick));
-            menu.MenuItems.Add("Copyright", new EventHandler(ShowC));
-            menu.MenuItems.Add("Exit", new EventHandler(ExitClick));
+            LoadProxys();
+            BuildMenu();
             notifyIcon.ContextMenu = menu;
             notifyIcon.Click += Click;
             SetTimer();
+            dgProxy.ItemsSource = proxys;
         }
 
         /// <summary>
@@ -54,9 +61,9 @@ namespace ProxyTrayIndicator
             if (((System.Windows.Forms.MouseEventArgs)e).Button == System.Windows.Forms.MouseButtons.Left)
             {
                 if (proxySet)
-                    Microsoft.Win32.Registry.SetValue(key, name, 0, Microsoft.Win32.RegistryValueKind.DWord);
+                    Microsoft.Win32.Registry.SetValue(key, regProxyEnable, 0, Microsoft.Win32.RegistryValueKind.DWord);
                 else
-                    Microsoft.Win32.Registry.SetValue(key, name, 1, Microsoft.Win32.RegistryValueKind.DWord);
+                    Microsoft.Win32.Registry.SetValue(key, regProxyEnable, 1, Microsoft.Win32.RegistryValueKind.DWord);
             }
         }
 
@@ -76,7 +83,7 @@ namespace ProxyTrayIndicator
         /// </summary>
         private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
-            if ((int)Microsoft.Win32.Registry.GetValue(key, name, 8) == 1)
+            if ((int)Microsoft.Win32.Registry.GetValue(key, regProxyEnable, 8) == 1)
             {
                 proxySet = true;
                 notifyIcon.Icon = Resource.on;
@@ -89,20 +96,91 @@ namespace ProxyTrayIndicator
             notifyIcon.Text = (string)Microsoft.Win32.Registry.GetValue(key, "ProxyServer", "no proxy server");
         }
 
-        private void ExitClick(object sender, EventArgs e) =>
+        private void ExitClick(object sender, EventArgs e)
+        {
+            this.close = true;
             this.Close();
+        }
 
         private void ShowC(object sender, EventArgs e) =>
-            MessageBox.Show(Resource.License, "Copyrights and licenses");
+            System.Windows.Forms.MessageBox.Show(Resource.License, "Copyrights and licenses");
 
         private void LaunchIEParamClick(object sender, EventArgs e) =>
             proc.Start();
 
-        private void OnClose(Object sender, EventArgs e)
+        private void EventClosing(Object sender, System.ComponentModel.CancelEventArgs e)
         {
+            if (! this.close)
+            {
+                UpdateProxyMenu();
+                this.Hide();
+                this.ShowInTaskbar = false;
+                e.Cancel = true;
+            }
+        }
+        private void EventClosed(Object sender, EventArgs e)
+        {
+            SaveProxys();
             mutex.ReleaseMutex();
             notifyIcon.Dispose();
             return;
+        }
+
+
+        private void BuildMenu()
+        {
+            menu.MenuItems.Add("Exit", new EventHandler(ExitClick));
+            menu.MenuItems.Add("Copyright & sources", new EventHandler(ShowC));
+            menu.MenuItems.Add("Show IE settings", new EventHandler(LaunchIEParamClick));
+            menu.MenuItems.Add("Edit proxys", new EventHandler(EditProxy));
+            UpdateProxyMenu();
+        }
+
+        private void UpdateProxyMenu()
+        {
+            proxyMenu.MenuItems.Clear();
+            proxyMenu.MenuItems.AddRange(GetProxyList());
+            menu.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] { proxyMenu });
+        }
+
+        private System.Windows.Forms.MenuItem[] GetProxyList()
+        {
+            List<System.Windows.Forms.MenuItem> menu = new List<System.Windows.Forms.MenuItem>();
+            foreach (Proxy proxy in proxys)
+            {
+                menu.Add(new System.Windows.Forms.MenuItem(proxy.Name, SetProxy) { Tag = proxy });
+            }
+            return menu.ToArray();
+        }
+
+        private void SetProxy(Object sender, EventArgs e)
+        {
+            System.Windows.Forms.MenuItem item = sender as System.Windows.Forms.MenuItem;
+            if (item != null)
+            {
+                Proxy proxy = item.Tag as Proxy;
+                if (proxy != null)
+                {
+                    Microsoft.Win32.Registry.SetValue(key, regProxyServer, String.Format("{0}:{1}", proxy.Address, proxy.Port), Microsoft.Win32.RegistryValueKind.String);
+                }
+            }
+        }
+
+        private void EditProxy(Object sender, EventArgs e)
+        {
+            this.Show();
+            this.ShowInTaskbar = true;
+        }
+
+        private void SaveProxys()
+        {
+            Serializer.Save(savePath, proxys);
+        }
+
+        private void LoadProxys()
+        {
+            if (File.Exists(savePath))
+                proxys = Serializer.Load<List<Proxy>>(savePath);
         }
     }
 }
---
+++ ProxyTrayIndicator/Proxy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyTrayIndicator
+{
+    [Serializable]
+    public class Proxy
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Port { get; set; }
+    }
+}
